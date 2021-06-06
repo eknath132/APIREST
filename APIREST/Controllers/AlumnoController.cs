a80b7f1f44d9aa13df4d63de4dfe252d71057525
renzo <perales.azarel@gmail.com>
2021-06-06 16:53:31 -0300

    Agregar archivos de proyecto.

---
+++ APIREST/Controllers/AlumnoController.cs
@@ -0,0 +1,32 @@
+﻿using ApiRest.Modelos;
+using ApiRest.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlumnoController : ControllerBase
+    {
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Alumnos Alumno = new Alumnos();
+            return Ok(Alumno.ObtenerAlumno());
+        }
+
+        [HttpPost("agregar")]
+        public IActionResult AgregarAlumno(Alumno nuevoAlumno)
+        {
+            Alumnos alumno = new Alumnos();
+            alumno.Agregar(nuevoAlumno);
+            return CreatedAtAction(nameof(AgregarAlumno), nuevoAlumno);
+        }
+    }
+}
---
+++ APIREST/Repositorio/Alumnos.cs
@@ -0,0 +1,28 @@
+﻿using ApiRest.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRest.Repositorio
+{
+    public class Alumnos
+    {
+        public static List<Alumno> _listaAlumnos = new List<Alumno>()
+        {
+            new Alumno() { Id = 1, Nombre = "Alumno 1" , Apellido = "Apellido 1" },
+            new Alumno() { Id = 2, Nombre = "Alumno 2" , Apellido = "Apellido 2" },
+            new Alumno() { Id = 3, Nombre = "Alumno 3" , Apellido = "Apellido 3" }
+        };
+
+        public IEnumerable<Alumno> ObtenerAlumno()
+        {
+            return _listaAlumnos;
+        }
+
+        public void Agregar(Alumno nuevoAlumno)
+        {
+            _listaAlumnos.Add(nuevoAlumno);
+        }
+    }
+}
